52d11884e01f63698525dd73a43344f936b380d5
Jonathan de Decker <jodedeck542@gmail.com>
2019-03-12 11:23:05 +0100

    ShoppingCart, JasonWebApiConfig, ShoppingProductJson, migrations

--- Fnuc.DAL/Migrations/Configuration.cs
+++ Fnuc.DAL/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace Fnuc.DAL.Migrations
 {
     using Fnuc.DAL.Entities;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -21,16 +22,32 @@ namespace Fnuc.DAL.Migrations
             //  to avoid creating duplicate seed data.
             Product product = new Product
             {
-                Name = "keyboard",
-                Description = "Razor Keyboard v3",
-                Price = 50,
+                Name = "Ikea gamer desk v2",
+                Description = "Gamming desk",
+                Price = 250,
                 PublicationDate = DateTime.Now,
-                StockQuantity = 15,
+                StockQuantity = 5,
                 CategoryId = 3,
             };
 
             context.Products.Add(product);
 
+            //var categories = new List<Category>()
+            //{        
+            //    new Category {Id = 1, Name = "furniture", ParentCategoryId=null },
+            //    new Category {Id = 2, Name = "desk", ParentCategoryId=1 },
+            //    new Category {Id = 3, Name = "gamer desk", ParentCategoryId=2}
+            //};  
+
+            //var categories = new List<Category>()
+            //{
+            //    new Category {Name = "chair", ParentCategoryId=1 },
+            //    new Category {Name = "storage", ParentCategoryId=1 },
+            //    new Category {Name = "table", ParentCategoryId=1}
+            //};
+
+            //categories.ForEach(c => context.Categories.Add(c));
+            //context.SaveChanges();
         }
     }
 }
